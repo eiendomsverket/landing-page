5e44c4e5895f262905e7e66073490273937e3af0
mosti <mosti4@hotmail.com>
2025-06-29 23:37:40 +0200

    Added landing page layout from mui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+'use client';
 import React from 'react';
-import Blog from './blog/Blog';
+// import Blog from './blog/Blog';
+import MaretingPage from '../app/marketing-page/MarketingPage';
 export default function Home() {
   return (
-
-      <Blog/>
+      // <Blog/>
+      <MaretingPage/>
   );
 }
